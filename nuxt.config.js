530dc36993c92e5ae44603d30a6795f3429c284a
Burton Smith <bsmith@amazazing.com>
2019-05-02 19:30:55 -0400

    renamed styles directory to "scss".

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,7 +95,7 @@ module.exports = {
     /*
      ** Global CSS
      */
-    css: ['@/assets/sass/styles.scss'],
+    css: ['@/assets/scss/styles.scss'],
 
     /*
      ** Plugins to load before mounting the App
