37dc07c4c2b76fe14074ff0bf6dc5e2ed5e8bd33
break-stuff <burton@breakstuff.com>
2019-06-29 17:07:43 -0400

    updated deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'www.projectclarion.com' > CNAME
+echo 'projectclarion.com' > CNAME
 
 git init
 git add -A
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:break-stuff/clarion.git master:gh-pages
+git push -f https://github.com/break-stuff/clarion.git master:gh-pages
 
 cd -
