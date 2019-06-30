338fbdacc7816e04600b31c5a770faae701794bd
break-stuff <burton@breakstuff.com>
2019-06-29 22:12:07 -0400

    Update sitemap/plugin config and metadata

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,28 @@
 module.exports = {
     title: 'Clarion',
-    description: 'See how the Clarion CLI and style framework can make managing styles in your project simpler.',
+    description: 'See how the Clarion CLI and style framework can make managing styles and design systems simpler.',
     head: [
-        ['script', { src: 'https://kit.fontawesome.com/24018dd45e.js' }]
+        ['link', { rel: 'icon', href: '/images/favicon.png' }],
+        [
+            'meta',
+            { property: 'og:image', content: '/images/clarion_default_image.png' },
+            { property: 'og:title', content: 'Clarion CLI and Style Framework' },
+            { property: 'og:description', content: 'See how the Clarion CLI and style framework can make managing styles and design systems simpler.' },
+            { property: 'twitter:image', content: '/images/clarion_default_image.png' },
+            { property: 'twitter:card', content: 'summary_large_image' },
+            { property: 'twitter:creator', content: '@stuffbreaker' },
+            { property: 'robots', content: 'index, follow' },
+            { property: 'HandheldFriendly', content: 'True' },
+        ]
     ],
-    plugins: [
-        [ 
-            '@vuepress/google-analytics',
-            {
-                'ga': 'UA-106134260-1'
-            }
-        ]  
-    ],
+    plugins: {
+        '@vuepress/google-analytics': {
+            'ga': 'UA-106134260-1'
+        },
+        'sitemap': {
+            hostname: 'https://projectclarion.com'
+        }
+    },
     themeConfig: {
         nav: [
             { text: 'Contact', link: '/contact.html' },
@@ -25,44 +36,44 @@ module.exports = {
                 children: [
                     {
                         title: 'Documentation',
-                        children: [ 
+                        children: [
                             '/cli/documentation/new.html',
                             '/cli/documentation/add.html',
                             '/cli/documentation/configuration.html',
                         ]
-                    },       
+                    },
                     {
                         title: 'Examples',
-                        children: [ 
+                        children: [
                             '/cli/examples/angular.html',
                             '/cli/examples/aspnet.html',
                             '/cli/examples/bootstrap.html',
                             '/cli/examples/react.html',
                             '/cli/examples/vue.html',
                         ]
-                    }        
+                    }
                 ]
             },
             {
                 title: 'Framework',
-                children: [ 
+                children: [
                     {
                         title: 'Documentation',
-                        children: [ 
+                        children: [
                             '/framework/documentation/architecture.html',
                             '/framework/documentation/variables.html',
                             {
                                 title: 'Functions',
-                                children: [ 
+                                children: [
                                     '/framework/documentation/functions/colors.html',
                                     '/framework/documentation/functions/spacing.html',
                                     '/framework/documentation/functions/strings.html',
                                     '/framework/documentation/functions/z-index.html',
                                 ]
-                            },     
+                            },
                             {
                                 title: 'Mixins',
-                                children: [ 
+                                children: [
                                     '/framework/documentation/mixins/border-radius.html',
                                     '/framework/documentation/mixins/breakpoints.html',
                                     '/framework/documentation/mixins/display.html',
@@ -72,7 +83,7 @@ module.exports = {
                                 ]
                             }
                         ]
-                    }        
+                    }
                 ]
             }
         ]
